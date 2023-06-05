231a6b4a650adb717a37aa3be8fd41338f1e7f38
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-06-05 13:45:20 -0300

    cambios

--- Modulo2/U2M2/mostrar_contenido.php
+++ Modulo2/U2M2/mostrar_contenido.php
@@ -2,43 +2,51 @@
 
 ?>
 
-<div class= "contenido">
-
-<?php 
-session_start();
-?>
-
-
-<div class ="salir">
-<ul class="menu_salir">
- <li> <a href="salir.php">Salir</a></li> </li>
-</ul>
-</div>
-
-<div class="mostrar_imagen">
-    <img src="img/acceso_cliente.png"  alt="">
-</div>
-
- 
-
-
-</div>
-
-<?php
-    include("conexion.php");
-
-    $consulta_db = mysqli_query($conexion_db, "SELECT * FROM autorizados");
-
-    while ($mostrar_datos = mysqli_fetch_assoc($consulta_db)) {
-        
-    ?>
-
-        <div class="mostrar_autorizados">
-            <h3><?php echo "Bienvenido cliente DNI: " . $mostrar_datos["usuario"]; ?></h3>
-            <h3>En esta sección encontrará información sobre sus pedidos y compras</h3>
-        </div>
-    <?php }?>
+<div class="contenido">
 
     <?php
+    session_start();
+    ?>
 
-// va if y else pero no se como//
+
+    <div class="salir">
+        <ul class="menu_salir">
+            <li> <a href="salir.php">Salir</a></li>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mostrar_imagen">
+        <img src="img/acceso_cliente.png" alt="">
+    </div>
+
+
+
+
+</div>
+
+
+<?php
+include("conexion.php");
+
+
+$consulta_db = mysqli_query($conexion_db, "SELECT * FROM autorizados WHERE usuario = '$dni_cliente'");
+while ($mostrar_datos = mysqli_fetch_assoc($consulta_db)) {
+    echo "Bienvenido cliente DNI: " . $mostrar_datos["usuario"] . ". En esta sección encontrará información sobre sus pedidos y compras.";
+}
+
+
+?>
+
+<!-- 
+Asegúrate de reemplazar $dni_cliente con el valor real del DNI del cliente al que deseas saludar.
+ De esta manera, la consulta SQL solo recuperará los datos correspondientes a ese cliente en particular,
+  y el mensaje de bienvenida se mostrará solo una vez para ese cliente. -->
+
+<!--  $consulta_db = mysqli_query($conexion_db, "SELECT * FROM autorizados");
+
+while ($mostrar_datos = mysqli_fetch_assoc($consulta_db)) {
+$dni_cliente = $mostrar_datos["usuario"];
+echo "Bienvenido cliente DNI: " . $dni_cliente . ". En esta sección encontrará información sobre sus pedidos y compras.";
+}
+     -->
